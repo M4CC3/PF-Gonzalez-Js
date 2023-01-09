845bd13d6b09800b6ca9ae492e8a97e0dd7abc78
M4CC3 <mati_gonzalez15@hotmail.com>
2023-01-09 19:32:58 -0300

    nueva moneda= real br

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ function convertir() {
     let valore = parseInt(document.getElementById("valor").value);
     let resultado = 0;
     let dolar = 185;/*ACTUALIZAR DOLAR*/
-    let euro = 193;/*ACTUALIZAR EURO*/
+    let real = 34;/*ACTUALIZAR REAL*/
     if (document.getElementById("uno").checked) {
         resultado = valore / dolar;
         alert("El cambio de Pesos a Dolares es: US$ " + resultado.toFixed(2));
     }
     else if (document.getElementById("dos").checked) {
-        resultado = valore / euro;
-        alert("El cambio de Pesos a Euros es: € " + resultado.toFixed(2))
+        resultado = valore / real;
+        alert("El cambio de Pesos a Reales es:  R$ " + resultado.toFixed(2))
     }
     else {
         alert("Por favor, selecciona un tipo de cambio. ")
