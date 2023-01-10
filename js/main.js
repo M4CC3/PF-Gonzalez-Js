a8b1e77fd09d4af4c63321937a4b3f39f2d99e51
M4CC3 <mati_gonzalez15@hotmail.com>
2023-01-10 15:25:58 -0300

    agregada 3er moneda

--- js/main.js
+++ js/main.js
@@ -3,13 +3,18 @@ function convertir() {
     let resultado = 0;
     let dolar = 185;/*ACTUALIZAR DOLAR*/
     let real = 34;/*ACTUALIZAR REAL*/
+    let pesoUruguayo = 4.53;/*ACTUALIZAR URU*/
     if (document.getElementById("uno").checked) {
         resultado = valore / dolar;
-        alert("El cambio de Pesos a Dolares es: US$ " + resultado.toFixed(2));
+        alert("Con el cambio oficial serian: US$ " + resultado.toFixed(2));
     }
     else if (document.getElementById("dos").checked) {
         resultado = valore / real;
-        alert("El cambio de Pesos a Reales es:  R$ " + resultado.toFixed(2))
+        alert("Con el cambio oficial serian: R$ " + resultado.toFixed(2))
+    }
+    else if (document.getElementById("tres").checked) {
+        resultado = valore / pesoUruguayo;
+        alert("Con el cambio oficial serian: $ " + resultado.toFixed(2))
     }
     else {
         alert("Por favor, selecciona un tipo de cambio. ")
